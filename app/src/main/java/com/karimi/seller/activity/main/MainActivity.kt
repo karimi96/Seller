37e2ddfc74a6d9415ef5b10767779eeede9cfd2a
karimi <zmanyamyd5@gmail.com>
2022-08-03 00:28:32 +0430

    set splash Activity for luncher

---
+++ app/src/main/java/com/karimi/seller/activity/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
