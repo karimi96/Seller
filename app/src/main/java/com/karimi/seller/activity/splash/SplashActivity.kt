ba6b984ba58812f775cf9d3a427347c859279c3e
karimi <zmanyamyd5@gmail.com>
2022-08-03 00:16:42 +0430

    create splash

---
+++ app/src/main/java/com/karimi/seller/activity/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
