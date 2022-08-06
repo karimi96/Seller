e6002835e7c1326eb693e387532e35fc28cbb99d
karimi <zmanyamyd5@gmail.com>
2022-08-06 18:17:38 +0430

    create FirstOpenActivity

---
+++ app/src/main/java/com/karimi/seller/activity/business_add/FirstOpenActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.business_add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class FirstOpenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_open)
+    }
+}
