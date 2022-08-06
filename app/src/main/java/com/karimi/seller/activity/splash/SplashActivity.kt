cbcfe4673e08af5b351bee290a5e695a247c9ae5
karimi <zmanyamyd5@gmail.com>
2022-08-06 18:18:17 +0430

    set runtimePermision

--- app/src/main/java/com/karimi/seller/activity/splash/SplashActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/splash/SplashActivity.kt
@@ -1,12 +1,114 @@
 package com.karimi.seller.activity.splash
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.karimi.seller.R
+import com.karimi.seller.activity.business_add.FirstOpenActivity
+import com.karimi.seller.activity.main.MainActivity
+import com.karimi.seller.helper.Config
+import com.karimi.seller.helper.Session
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        start()
+//        DownloadDataSampleDialog(this).show(supportFragmentManager,"download_data")
+//        Log.e("qqqq", "onCreate: ${Session.getInstance().sessionKey}" )
     }
+
+    private fun start(){
+        if(checkPermission()){
+            Handler().postDelayed({
+                val i = if (Session.getInstance().sessionKey.isNullOrEmpty()){
+                    Intent(this, FirstOpenActivity::class.java)
+                } else Intent(this, MainActivity::class.java)
+                startActivity(i)
+                finish()
+            },1500)
+        }
+    }
+
+
+    private val neededPermissions = arrayOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        Manifest.permission.READ_EXTERNAL_STORAGE)
+    //    Permos
+    private fun checkPermission(): Boolean {
+        val currentAPIVersion = Build.VERSION.SDK_INT
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            val permissionsNotGranted = ArrayList<String>()
+            for (permission in neededPermissions) {
+                if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                    permissionsNotGranted.add(permission)
+                }
+            }
+            if (permissionsNotGranted.size > 0) {
+                var shouldShowAlert = false
+                for (permission in permissionsNotGranted) {
+                    shouldShowAlert = ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+                }
+
+                val arr = arrayOfNulls<String>(permissionsNotGranted.size)
+                val permissions = permissionsNotGranted.toArray(arr)
+                if (shouldShowAlert) {
+                    showPermissionAlert(permissions)
+                } else {
+                    requestPermissions(permissions)
+                }
+                return false
+            }
+        }
+        return true
+    }
+
+
+    private fun showPermissionAlert(permissions: Array<String?>) {
+        val alertBuilder = AlertDialog.Builder(this)
+        alertBuilder.setCancelable(false)
+        alertBuilder.setTitle(R.string.permission_required)
+        alertBuilder.setMessage(R.string.permission_message)
+        alertBuilder.setPositiveButton(R.string.positive_button) { _, _ -> requestPermissions(permissions) }
+        alertBuilder.setNegativeButton(R.string.negative_button) { _, _ -> finish() }
+        val alert = alertBuilder.create()
+        alert.show()
+    }
+
+    private fun requestPermissions(permissions: Array<String?>) {
+        ActivityCompat.requestPermissions(this, permissions, Config.REQUEST_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            Config.REQUEST_CODE -> {
+                for (result in grantResults) {
+                    Log.e("qqqRequestPelt", "" + result)
+                    if (result == PackageManager.PERMISSION_DENIED) {
+                        Log.e("qqqRequestPelt", "PERMISSION_DENIED $result")
+
+                        // Not all permissions granted. Show some message and return.
+                        return
+                    }else{
+                        Log.e("qqqRequestPelt", "PERMISSION $result")
+                    }
+                    start()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+
 }
