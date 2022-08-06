a98165abf23c19ce1d6ae53b7278fda82ebca382
karimi <zmanyamyd5@gmail.com>
2022-08-06 18:16:22 +0430

    create Config and App class

---
+++ app/src/main/java/com/karimi/seller/helper/App.kt
@@ -0,0 +1,23 @@
+package com.karimi.seller.helper
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.support.multidex.MultiDex
+import android.support.multidex.MultiDexApplication
+
+class App : MultiDexApplication() {
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+        MultiDex.install(this)
+    }
+
+    companion object {
+        @SuppressLint("StaticFieldLeak")
+        lateinit var context: Context
+    }
+
+
+
+    }
---
+++ app/src/main/java/com/karimi/seller/helper/Config.kt
@@ -0,0 +1,7 @@
+package com.karimi.seller.helper
+
+object Config {
+    const val REQUEST_CODE = 100
+    const val SESSION_KEY = "SESSION_KEY"
+
+}
