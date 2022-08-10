479d87cf4960f3dabed9ba8db592ca698390f731
karimi <zmanyamyd5@gmail.com>
2022-08-10 19:54:31 +0430

    fix design

--- app/src/main/java/com/karimi/seller/activity/business_add/FirstOpenActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/business_add/FirstOpenActivity.kt
@@ -1,12 +1,17 @@
 package com.karimi.seller.activity.business_add
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import kotlinx.android.synthetic.main.box_first_activity.*
 
 class FirstOpenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_open)
+
+        var typeface = Typeface.createFromAsset(assets,"font/iran_sans.ttf")
+        description.typeface = typeface
     }
 }
