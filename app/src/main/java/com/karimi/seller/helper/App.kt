ef52a8d6078a05f1a09260235149b4748070a599
karimi <zmanyamyd5@gmail.com>
2022-08-14 11:19:05 +0430

    add some function to App class

--- app/src/main/java/com/karimi/seller/helper/App.kt
+++ app/src/main/java/com/karimi/seller/helper/App.kt
@@ -2,22 +2,41 @@ package com.karimi.seller.helper
 
 import android.annotation.SuppressLint
 import android.content.Context
-import android.support.multidex.MultiDex
-import android.support.multidex.MultiDexApplication
+import android.widget.EditText
+import android.widget.Toast
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
+import com.karimi.seller.database.AppDatabase
 
 class App : MultiDexApplication() {
 
     override fun onCreate() {
         super.onCreate()
         context = this
+        database = AppDatabase.getInstance(this)
         MultiDex.install(this)
     }
 
     companion object {
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
+        lateinit var database: AppDatabase
+
+
+        fun branch(): Int {
+            return Session.getInstance().branch
+        }
+
+
+        fun toast(message: String) {
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+        }
+
+
+        fun getString(editText: EditText): String {
+            return editText.text.toString().trim()
+        }
+
+
     }
-
-
-
-    }
+}
