131704918cd148ddfc6ecf01b1b6015deefb3392
karimi <zmanyamyd5@gmail.com>
2022-08-17 12:04:06 +0430

    fix

---
+++ app/src/main/java/com/karimi/seller/adapter/OrderWaitingAdapter.kt
@@ -0,0 +1,59 @@
+package com.karimi.seller.adapter
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.karimi.seller.R
+import com.karimi.seller.helper.App
+import com.karimi.seller.model.Orders
+import kotlinx.android.synthetic.main.item_order_waiting.view.*
+
+
+class OrderWaitingAdapter(
+    val context: Context,
+    private val list: ArrayList<Orders>,
+    private val listener: Listener?
+): RecyclerView.Adapter<OrderWaitingAdapter.ListViewHolder>() {
+
+
+    class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    interface Listener{
+        fun onItemClicked(position: Int, item: Orders)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_order_waiting, parent, false)
+        return ListViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
+        val item = holder.itemView
+        val model = list[position]
+
+        item.title.text = if (model.customer_name.isNullOrEmpty()) "سفارش #${model.id}" else model.customer_name
+        item.desc.text = App.priceFormat(model.totla_all)
+
+        if (listener != null) item.setOnClickListener { listener.onItemClicked(position, model) }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun addItem(item: Orders){
+        list.add(item)
+        notifyDataSetChanged()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateList(items: ArrayList<Orders>){
+        this.list.clear()
+        this.list.addAll(items)
+        notifyDataSetChanged()
+    }
+}
