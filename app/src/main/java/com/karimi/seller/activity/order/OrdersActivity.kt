e47e227cbe78a4a6d7e187b3f33729c97c8548df
karimi <zmanyamyd5@gmail.com>
2022-08-21 18:02:25 +0430

    create OrderActivity and set design

---
+++ app/src/main/java/com/karimi/seller/activity/order/OrdersActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.order
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class OrdersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_orders)
+    }
+}
