4b32966c97299cb9bda8cb0daef055c8bcac3707
karimi <zmanyamyd5@gmail.com>
2022-08-21 18:07:43 +0430

    create CustomerActivity and fix design

---
+++ app/src/main/java/com/karimi/seller/activity/customer/CustomerActivity.kt
@@ -0,0 +1,14 @@
+package com.karimi.seller.activity.customer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class CustomerActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer)
+    }
+}
