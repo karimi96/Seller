7ab202b7d7b71b41bd153e2d49a46d61a7b2985c
karimi <zmanyamyd5@gmail.com>
2022-08-21 18:03:58 +0430

    create CategoryActivity and fix design

---
+++ app/src/main/java/com/karimi/seller/activity/category/CategoryActivity.kt
@@ -0,0 +1,18 @@
+package com.karimi.seller.activity.category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class CategoryActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+
+
+
+
+    }
+}
