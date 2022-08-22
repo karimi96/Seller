3e5a030a589609c18ef1b01af24175b9ac813bb1
karimi <zmanyamyd5@gmail.com>
2022-08-22 15:54:26 +0430

    create Activity and fix design

---
+++ app/src/main/java/com/karimi/seller/activity/gozareshAnbar/AnbarActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.gozareshAnbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class AnbarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anbar)
+    }
+}
