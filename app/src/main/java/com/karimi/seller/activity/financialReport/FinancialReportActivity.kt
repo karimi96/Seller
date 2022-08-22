52e1f3dfd3cba242c9eeb52d4ae2bc8fcc414390
karimi <zmanyamyd5@gmail.com>
2022-08-22 16:02:53 +0430

    create FinancialReportActivity

---
+++ app/src/main/java/com/karimi/seller/activity/financialReport/FinancialReportActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.financialReport
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class FinancialReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_financial_report)
+    }
+}
