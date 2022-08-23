b3b58acfb5b169706631cfe9ccbd0a2bbcfe53f5
karimi <zmanyamyd5@gmail.com>
2022-08-23 12:50:05 +0430

    create AddNewProduct Activity

---
+++ app/src/main/java/com/karimi/seller/activity/product/AddNewProductActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.product
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class AddNewProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_new_product)
+    }
+}
