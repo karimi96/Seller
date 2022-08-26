ed282eb18d9616fa9cc570281bd4d11ca8725227
karimi <zmanyamyd5@gmail.com>
2022-08-26 23:45:24 +0430

    check intent

--- app/src/main/java/com/karimi/seller/activity/customer/CustomerViewActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/customer/CustomerViewActivity.kt
@@ -6,9 +6,16 @@ import com.karimi.seller.R
 
 class CustomerViewActivity : AppCompatActivity() {
 
+    var customer_id : Int? = null
+    var position : Int? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_view)
+
+        if (intent.extras != null){
+            customer_id = intent.getIntExtra("product_id",-1)
+            position = intent.getIntExtra("pos",-1)
+        }
     }
 }
