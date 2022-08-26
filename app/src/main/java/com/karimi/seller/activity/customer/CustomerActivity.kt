831f0bdf927ce041f3c85bd2cdacca2f57ba9bb1
karimi <zmanyamyd5@gmail.com>
2022-08-26 19:27:14 +0430

    add CustomerListAdapter in Activity

--- app/src/main/java/com/karimi/seller/activity/customer/CustomerActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/customer/CustomerActivity.kt
@@ -3,9 +3,11 @@ package com.karimi.seller.activity.customer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import com.karimi.seller.adapter.CustomerListAdapter
 
 class CustomerActivity : AppCompatActivity() {
 
+    private var adapter : CustomerListAdapter? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
