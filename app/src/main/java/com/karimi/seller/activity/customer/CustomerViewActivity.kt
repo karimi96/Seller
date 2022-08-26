6ebad7a25afe32d32c7e6ba47becdf5bab51c0aa
karimi <zmanyamyd5@gmail.com>
2022-08-26 23:48:31 +0430

    get Customer from database by id

--- app/src/main/java/com/karimi/seller/activity/customer/CustomerViewActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/customer/CustomerViewActivity.kt
@@ -3,11 +3,16 @@ package com.karimi.seller.activity.customer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import com.karimi.seller.helper.App
+import com.karimi.seller.model.Customers
 
 class CustomerViewActivity : AppCompatActivity() {
 
     var customer_id : Int? = null
     var position : Int? = null
+
+    private var this_customer : Customers = Customers()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,5 +22,16 @@ class CustomerViewActivity : AppCompatActivity() {
             customer_id = intent.getIntExtra("product_id",-1)
             position = intent.getIntExtra("pos",-1)
         }
+
+
+        this_customer = App.database.getAppDao().selectCustomerById(customer_id!!)!!
+
+
+
+
+
+
+
+
     }
 }
