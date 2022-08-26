e549455d3ad3d3193a4547af572aca43c882a742
karimi <zmanyamyd5@gmail.com>
2022-08-26 19:28:08 +0430

    add TagInfoAdapter in Activity

--- app/src/main/java/com/karimi/seller/activity/customer/CustomerActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/customer/CustomerActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
 import com.karimi.seller.adapter.CustomerListAdapter
+import com.karimi.seller.adapter.TagInfoAdapter
 
 class CustomerActivity : AppCompatActivity() {
 
     private var adapter : CustomerListAdapter? =null
+    private var adapterTag : TagInfoAdapter? =null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
