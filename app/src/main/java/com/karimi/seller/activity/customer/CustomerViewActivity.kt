7b74a13511a14f5ec396798b07980e7af398a4c4
karimi <zmanyamyd5@gmail.com>
2022-08-27 00:35:30 +0430

    init data

--- app/src/main/java/com/karimi/seller/activity/customer/CustomerViewActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/customer/CustomerViewActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import com.karimi.seller.R
 import com.karimi.seller.helper.App
 import com.karimi.seller.model.Customers
+import kotlinx.android.synthetic.main.include_toolbar_customer_view.*
+import java.util.*
 
 class CustomerViewActivity : AppCompatActivity() {
 
@@ -26,12 +28,28 @@ class CustomerViewActivity : AppCompatActivity() {
 
         this_customer = App.database.getAppDao().selectCustomerById(customer_id!!)!!
 
-
-
-
-
-
+        initData()
 
 
     }
+
+
+    private fun initData(){
+//        tv_id.setText("#${this_customer?.id}")
+        tv_customer_name.text = this_customer?.name
+
+        tv_order_count.setText("700 سفارش")
+        tv_amount_all_order.setText(App.priceFormat(28000000.0,true))
+        tv_order_last_date.setText(App.getFormattedDate(Date()))
+
+        try {
+            tv_date.setText("در تاریخ ${App.getFormattedDate(this_customer?.updated_at)}ویرایش شده \n در تاریخ${App.getFormattedDate(this_customer?.created_at)} ثبت شده")
+        }catch (e:Exception){
+
+        }
+    }
+
+
+
+
 }
