0ce8a35504b8e7dc29e47e6ce3eddc8ee185696b
karimi <zmanyamyd5@gmail.com>
2022-08-26 18:20:46 +0430

    create method convert string url to bite

--- app/src/main/java/com/karimi/seller/helper/App.kt
+++ app/src/main/java/com/karimi/seller/helper/App.kt
@@ -2,13 +2,19 @@ package com.karimi.seller.helper
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.net.Uri
+import android.os.Environment
+import android.util.Log
 import android.widget.EditText
 import android.widget.Toast
 import androidx.multidex.MultiDex
 import androidx.multidex.MultiDexApplication
 import com.karimi.seller.database.AppDatabase
+import com.karimi.seller.helper.Config.JPG
+import com.karimi.seller.helper.Config.PATH_IMAGES
 import saman.zamani.persiandate.PersianDate
 import saman.zamani.persiandate.PersianDateFormat
+import java.io.*
 import java.text.DecimalFormat
 import java.util.*
 
@@ -60,6 +66,41 @@ class App : MultiDexApplication() {
             return PersianDateFormat("Y/m/d").format(PersianDate(date))
         }
 
+
+
+        fun getByte(uri: Uri): ByteArray{
+            val baos = ByteArrayOutputStream()
+            val fis: FileInputStream
+            try {
+                fis = FileInputStream(File(uri.getPath()))
+                val buf = ByteArray(1024)
+                var n: Int
+                while (-1 != fis.read(buf).also { n = it }) baos.write(buf, 0, n)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+            return baos.toByteArray()
+        }
+
+        fun saveFile(byteArray: ByteArray) : String{
+            val outStream: FileOutputStream
+            try {
+                val path = File(Environment.getExternalStorageDirectory(), PATH_IMAGES)
+                path.mkdirs()
+                val fileName = "image_${System.currentTimeMillis()}$JPG"
+                val file = File(path,fileName)
+                Log.e("qqqq", "saveFile: ${file.path}" )
+                outStream = FileOutputStream(file)
+                outStream.write(byteArray)
+                outStream.close()
+                return file.path
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            return ""
+        }
 
     }
 }
