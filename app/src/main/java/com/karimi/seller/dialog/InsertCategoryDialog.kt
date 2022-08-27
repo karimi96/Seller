3e64ad3df67d2237859a842f280d030b0d8e8aaa
karimi <zmanyamyd5@gmail.com>
2022-08-27 21:46:29 +0430

    fix

--- app/src/main/java/com/karimi/seller/dialog/InsertCategoryDialog.kt
+++ app/src/main/java/com/karimi/seller/dialog/InsertCategoryDialog.kt
@@ -27,7 +27,6 @@ class InsertCategoryDialog(
 
     init {
         setCancelable(true)
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,7 +55,7 @@ class InsertCategoryDialog(
     }
 
     private fun initOnClick() {
-        submit.btn.setOnClickListener {
+        submit_category.btn.setOnClickListener {
             if (formIsValid()) listener.insert(this, getValue(), _POS)
         }
 
--- app/src/main/java/com/karimi/seller/dialog/InsertCustomerDialog.kt
+++ app/src/main/java/com/karimi/seller/dialog/InsertCustomerDialog.kt
@@ -23,6 +23,7 @@ class InsertCustomerDialog(val _context: Context, val _customer: Customers?, val
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.dialog_add_customer, container, false)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
