bcd7d5ebb4d2e1d6c6a563a3546a3ce3484ed60b
karimi <zmanyamyd5@gmail.com>
2022-08-27 19:39:56 +0430

    create widget ChartPrice

---
+++ app/src/main/java/com/karimi/seller/widget/chart/ChartPrice.kt
@@ -0,0 +1,60 @@
+package com.karimi.seller.widget.chart
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.RelativeLayout
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.XAxis
+import com.github.mikephil.charting.data.BarData
+import com.github.mikephil.charting.data.BarDataSet
+import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
+import com.karimi.seller.R
+import kotlinx.android.synthetic.main.widget_chart_price.view.*
+
+class ChartPrice (context: Context?, attrs: AttributeSet?) : RelativeLayout(context, attrs) {
+
+    private var listener: Listener? = null
+
+    init {
+        View.inflate(context, R.layout.widget_chart_price, this)
+    }
+
+    fun setText(text:String){
+    }
+
+    interface Listener{
+    }
+
+
+    //    Test
+    private var barEntryArrayList: ArrayList<BarEntry> = ArrayList()
+    private var labelNames: ArrayList<String> = ArrayList()
+    fun barChartAdapter() {
+        for (i in 0 until 500) {
+            val month: String = "فروردین"
+            val sales: Int = i+10000*i
+            barEntryArrayList.add(BarEntry(i.toFloat(), sales.toFloat()))
+            labelNames.add(month)
+        }
+        val barDataSet = BarDataSet(barEntryArrayList, "فروش ماهانه")
+        barDataSet.color = resources.getColor(R.color.primary)
+        val description = Description()
+        description.setText("فروش ۱۵ روز اخیر")
+        barChart?.setDescription(description)
+        val barData = BarData(barDataSet)
+        barChart?.setData(barData)
+        val xAxis: XAxis = barChart?.xAxis!!
+        xAxis.valueFormatter = IndexAxisValueFormatter(labelNames)
+        xAxis.position = XAxis.XAxisPosition.TOP
+        xAxis.setDrawGridLines(false)
+        xAxis.setDrawAxisLine(false)
+        xAxis.granularity = 1f
+        xAxis.labelCount = labelNames.size
+        xAxis.labelRotationAngle = 270f
+        barChart.animateY(2000)
+        barChart.invalidate()
+    }
+
+}
