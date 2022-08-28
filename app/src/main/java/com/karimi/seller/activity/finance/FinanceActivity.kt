a569dacc2831f6dc96185f9806004fa7fd0fbcba
karimi <zmanyamyd5@gmail.com>
2022-08-28 18:32:00 +0430

    set adapter for orderList

--- app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
@@ -3,9 +3,13 @@ package com.karimi.seller.activity.finance
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import com.karimi.seller.adapter.OrderWaitingAdapter
 import com.karimi.seller.adapter.TagAdapter
+import com.karimi.seller.helper.App
+import com.karimi.seller.model.Orders
 import com.karimi.seller.model.TagList
 import kotlinx.android.synthetic.main.activity_customer_view.*
+import kotlinx.android.synthetic.main.include_financial_2.*
 import kotlinx.android.synthetic.main.include_toolbar_finance.*
 
 class FinanceActivity : AppCompatActivity() {
@@ -17,6 +21,7 @@ class FinanceActivity : AppCompatActivity() {
 
         chart_bar_price.barChartAdapter()
         initTagInfo()
+        initAdapterOrders()
 
 
 
@@ -32,6 +37,16 @@ class FinanceActivity : AppCompatActivity() {
         array_tag_info.add(TagList("تخفیف ۲۹۰,۰۰۰ تومان"))
         array_tag_info.add(TagList("هزینه ارسال ۲۹۰,۰۰۰ تومان"))
         recyclerView_tag_info.adapter = TagAdapter(this,array_tag_info,null)
+    }
+
+
+    private fun initAdapterOrders(){
+        recyclerView_order_financial.adapter = OrderWaitingAdapter(this, ArrayList(App.database.getAppDao().selectOrders(App.branch())),
+            object : OrderWaitingAdapter.Listener{
+                override fun onItemClicked(position: Int, item: Orders) {
+
+                }
+            })
     }
 
 }
