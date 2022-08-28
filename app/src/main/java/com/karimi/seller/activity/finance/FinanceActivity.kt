3bbaaaac7591a5a170088caa7f5e21c2e2446a92
karimi <zmanyamyd5@gmail.com>
2022-08-28 18:07:48 +0430

    set barChart

--- app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
@@ -3,12 +3,16 @@ package com.karimi.seller.activity.finance
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import kotlinx.android.synthetic.main.activity_customer_view.*
 
 class FinanceActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_financial_report)
+        setContentView(R.layout.activity_finance)
+
+        chart_bar_price.barChartAdapter()
+
     }
 }
