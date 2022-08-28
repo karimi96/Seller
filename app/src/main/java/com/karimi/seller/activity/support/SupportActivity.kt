e0e72e8ec492412cbaf8084f90edfc21fa432701
karimi <zmanyamyd5@gmail.com>
2022-08-28 19:41:57 +0430

    set action call

--- app/src/main/java/com/karimi/seller/activity/support/SupportActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/support/SupportActivity.kt
@@ -1,12 +1,22 @@
 package com.karimi.seller.activity.support
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import kotlinx.android.synthetic.main.activity_support.*
 
 class SupportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support)
+
+        tel.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse("tel:${getString(R.string.tel_action)}")
+            startActivity(i)
+        }
+
     }
 }
