fcb8376f3746c19cc15b13288947815ea4010993
karimi <zmanyamyd5@gmail.com>
2022-08-28 18:05:07 +0430

    rename

--- app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
@@ -1,10 +1,12 @@
-package com.karimi.seller.activity.financialReport
+package com.karimi.seller.activity.finance
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
 
-class FinancialReportActivity : AppCompatActivity() {
+class FinanceActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_financial_report)
