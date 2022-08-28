c6b7b90d18f9067d7df5b37d33eefb68b43ef778
karimi <zmanyamyd5@gmail.com>
2022-08-28 19:22:10 +0430

    create getData and setData methode

--- app/src/main/java/com/karimi/seller/activity/setting/SettingActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/setting/SettingActivity.kt
@@ -2,6 +2,7 @@ package com.karimi.seller.activity.setting
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.karimi.seller.R
 import com.karimi.seller.helper.App
 import com.karimi.seller.helper.Config.MONEY_TYPE_DEFAULT
@@ -9,6 +10,7 @@ import com.karimi.seller.helper.Session
 import com.karimi.seller.widget.text_watcher.PriceTextWatcher
 import kotlinx.android.synthetic.main.include_box_setting1.*
 import kotlinx.android.synthetic.main.include_box_setting2.*
+import kotlinx.android.synthetic.main.include_toolbar_setting.*
 
 class SettingActivity : AppCompatActivity() {
 
@@ -20,7 +22,10 @@ class SettingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setting)
 
         initData()
-
+        submit_setting.btn.setOnClickListener {
+            submit_setting.showLoader()
+            setData()
+        }
     }
 
     private fun initData(){
@@ -46,5 +51,36 @@ class SettingActivity : AppCompatActivity() {
         edt_free_shipping?.addTextChangedListener(PriceTextWatcher(edt_free_shipping) {})
     }
 
+
+    private fun setData(){
+        setting.min_order = App.convertToDouble(edt_min_order)
+        setting.tax = App.convertToInt(edt_tax)
+        setting.currency = App.getString(edt_money_type)
+        setting.shipping_price = App.convertToDouble(edt_price_shipping)
+        setting.shipping_free_on_order = App.convertToDouble(edt_free_shipping)
+        setting.sound_scanner = checkbox_sound_scanner.isChecked
+        setting.cash_discount = checkbox_discount.isChecked
+        setting.cash_money = checkbox_money.isChecked
+        setting.cash_card = checkbox_card.isChecked
+        setting.cash_debit = checkbox_debit.isChecked
+        saveData()
+    }
+
+
+    private fun saveData(){
+        App.database.getAppDao().insertSetting(setting)
+
+        Session.getInstance().minOrder = setting.min_order?:0.0
+        Session.getInstance().taxPercent = setting.tax?:0
+        Session.getInstance().moneyType = setting.currency
+        Session.getInstance().shippingPrice = setting.shipping_price?:0.0
+        Session.getInstance().freeShippingPrice = setting.shipping_free_on_order?:0.0
+
+        Session.getInstance().setCheckBox(setting.sound_scanner?:false,
+            setting.cash_money?:false, setting.cash_card?:false,
+            setting.cash_debit?:false, setting.cash_discount?:false)
+
+        Handler().postDelayed({finish()},700)
+    }
 
 }
