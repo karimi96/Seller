90377d60be10e462445000d21c877aa64a58871e
karimi <zmanyamyd5@gmail.com>
2022-08-28 18:25:11 +0430

    set adapter for recycler_tag

--- app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/finance/FinanceActivity.kt
@@ -3,7 +3,10 @@ package com.karimi.seller.activity.finance
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import com.karimi.seller.adapter.TagAdapter
+import com.karimi.seller.model.TagList
 import kotlinx.android.synthetic.main.activity_customer_view.*
+import kotlinx.android.synthetic.main.include_toolbar_finance.*
 
 class FinanceActivity : AppCompatActivity() {
 
@@ -13,6 +16,22 @@ class FinanceActivity : AppCompatActivity() {
         setContentView(R.layout.activity_finance)
 
         chart_bar_price.barChartAdapter()
+        initTagInfo()
+
+
+
 
     }
+
+
+    private fun initTagInfo(){
+        val array_tag_info = ArrayList<TagList>()
+        array_tag_info.add(TagList("درآمد نقدی ۲۹۰,۰۰۰ تومان"))
+        array_tag_info.add(TagList("درآمد کارتخوان ۲۹۰,۰۰۰ تومان"))
+        array_tag_info.add(TagList("نسیه ۲۹۰,۰۰۰ تومان"))
+        array_tag_info.add(TagList("تخفیف ۲۹۰,۰۰۰ تومان"))
+        array_tag_info.add(TagList("هزینه ارسال ۲۹۰,۰۰۰ تومان"))
+        recyclerView_tag_info.adapter = TagAdapter(this,array_tag_info,null)
+    }
+
 }
