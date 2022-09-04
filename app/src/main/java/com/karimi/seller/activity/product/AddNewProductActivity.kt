2a2b698a5509266958b67de8eeb5c697bbaac935
karimi <zmanyamyd5@gmail.com>
2022-09-04 11:48:25 +0430

    fix onBackPressed

--- app/src/main/java/com/karimi/seller/activity/product/AddNewProductActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/product/AddNewProductActivity.kt
@@ -3,10 +3,21 @@ package com.karimi.seller.activity.product
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
+import kotlinx.android.synthetic.main.toolbar_new_p.*
 
 class AddNewProductActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_product)
+
+        initToolbar()
     }
+
+
+    fun initToolbar(){
+        arrayOf(title_new_product,back_new_product).forEach { it.setOnClickListener { onBackPressed() } }
+    }
+    
 }
