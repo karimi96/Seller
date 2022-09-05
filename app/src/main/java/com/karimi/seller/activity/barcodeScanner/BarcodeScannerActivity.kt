8c81941534fc7ef0158a1f736236f058ee2f9d0e
karimi <zmanyamyd5@gmail.com>
2022-09-05 15:41:47 +0430

    create barcodScannerActivity

---
+++ app/src/main/java/com/karimi/seller/activity/barcodeScanner/BarcodeScannerActivity.kt
@@ -0,0 +1,12 @@
+package com.karimi.seller.activity.barcodeScanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.karimi.seller.R
+
+class BarcodeScannerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_barcode_scanner)
+    }
+}
