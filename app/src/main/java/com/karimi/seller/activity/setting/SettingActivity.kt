5a9bc7f20f5a42ca74b3c50de0f62e2cb6625096
karimi <zmanyamyd5@gmail.com>
2022-09-10 11:15:10 +0430

    fix id and set clickListener

--- app/src/main/java/com/karimi/seller/activity/setting/SettingActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/setting/SettingActivity.kt
@@ -22,37 +22,45 @@ class SettingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setting)
 
         initData()
+        initToolbar()
+    }
+
+
+    private fun initToolbar() {
+        arrayOf(title_setting, back_setting).forEach { it.setOnClickListener { onBackPressed() } }
+
         submit_setting.btn.setOnClickListener {
             submit_setting.showLoader()
             setData()
         }
     }
 
-    private fun initData(){
+
+    private fun initData() {
         initTextWatcherPrice()
 
-        edt_min_order.setText("${setting.min_order?:0}")
-        edt_tax.setText("${setting.tax?:0}")
-        edt_money_type.setText(setting.currency?:MONEY_TYPE_DEFAULT)
-        edt_price_shipping.setText("${setting.shipping_price?:0}")
-        edt_free_shipping.setText("${setting.shipping_free_on_order?:0}")
+        edt_min_order.setText("${setting.min_order ?: 0}")
+        edt_tax.setText("${setting.tax ?: 0}")
+        edt_money_type.setText(setting.currency ?: MONEY_TYPE_DEFAULT)
+        edt_price_shipping.setText("${setting.shipping_price ?: 0}")
+        edt_free_shipping.setText("${setting.shipping_free_on_order ?: 0}")
 
-        checkbox_sound_scanner.isChecked = setting.sound_scanner?:false
-        checkbox_discount.isChecked = setting.cash_discount?:false
-        checkbox_money.isChecked = setting.cash_money?:false
-        checkbox_card.isChecked = setting.cash_card?:false
-        checkbox_debit.isChecked = setting.cash_debit?:false
+        checkbox_sound_scanner.isChecked = setting.sound_scanner ?: false
+        checkbox_discount.isChecked = setting.cash_discount ?: false
+        checkbox_money.isChecked = setting.cash_money ?: false
+        checkbox_card.isChecked = setting.cash_card ?: false
+        checkbox_debit.isChecked = setting.cash_debit ?: false
     }
 
 
-    private fun initTextWatcherPrice(){
+    private fun initTextWatcherPrice() {
         edt_min_order?.addTextChangedListener(PriceTextWatcher(edt_min_order) {})
         edt_price_shipping?.addTextChangedListener(PriceTextWatcher(edt_price_shipping) {})
         edt_free_shipping?.addTextChangedListener(PriceTextWatcher(edt_free_shipping) {})
     }
 
 
-    private fun setData(){
+    private fun setData() {
         setting.min_order = App.convertToDouble(edt_min_order)
         setting.tax = App.convertToInt(edt_tax)
         setting.currency = App.getString(edt_money_type)
@@ -67,20 +75,22 @@ class SettingActivity : AppCompatActivity() {
     }
 
 
-    private fun saveData(){
+    private fun saveData() {
         App.database.getAppDao().insertSetting(setting)
 
-        Session.getInstance().minOrder = setting.min_order?:0.0
-        Session.getInstance().taxPercent = setting.tax?:0
+        Session.getInstance().minOrder = setting.min_order ?: 0.0
+        Session.getInstance().taxPercent = setting.tax ?: 0
         Session.getInstance().moneyType = setting.currency
-        Session.getInstance().shippingPrice = setting.shipping_price?:0.0
-        Session.getInstance().freeShippingPrice = setting.shipping_free_on_order?:0.0
+        Session.getInstance().shippingPrice = setting.shipping_price ?: 0.0
+        Session.getInstance().freeShippingPrice = setting.shipping_free_on_order ?: 0.0
 
-        Session.getInstance().setCheckBox(setting.sound_scanner?:false,
-            setting.cash_money?:false, setting.cash_card?:false,
-            setting.cash_debit?:false, setting.cash_discount?:false)
+        Session.getInstance().setCheckBox(
+            setting.sound_scanner ?: false,
+            setting.cash_money ?: false, setting.cash_card ?: false,
+            setting.cash_debit ?: false, setting.cash_discount ?: false
+        )
 
-        Handler().postDelayed({finish()},700)
+        Handler().postDelayed({ finish() }, 700)
     }
 
 }
