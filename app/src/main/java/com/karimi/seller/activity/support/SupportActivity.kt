5e93f7e4daf9ec281432be0a95f56996f47085cf
karimi <zmanyamyd5@gmail.com>
2022-09-10 11:06:48 +0430

    set listener

--- app/src/main/java/com/karimi/seller/activity/support/SupportActivity.kt
+++ app/src/main/java/com/karimi/seller/activity/support/SupportActivity.kt
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.karimi.seller.R
 import kotlinx.android.synthetic.main.activity_support.*
+import kotlinx.android.synthetic.main.toolbar_support.*
 
 class SupportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_support)
+
+        arrayOf(title_support, back_support).forEach { it.setOnClickListener { onBackPressed() } }
 
         tel.setOnClickListener {
             val i = Intent(Intent.ACTION_VIEW)
