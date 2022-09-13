fb14584c0589384b43fa15733b61f80150a45742
karimi <zmanyamyd5@gmail.com>
2022-09-13 13:18:51 +0430

    fix image of adapter

--- app/src/main/java/com/karimi/seller/adapter/ProductListAdapter_2.kt
+++ app/src/main/java/com/karimi/seller/adapter/ProductListAdapter_2.kt
@@ -6,11 +6,13 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.karimi.seller.R
 import com.karimi.seller.helper.App
 import com.karimi.seller.model.Product
+import kotlinx.android.synthetic.main.list_item_financial_report.view.*
 import kotlinx.android.synthetic.main.list_item_stock3.view.*
 
 
@@ -50,10 +52,18 @@ class ProductListAdapter_2(
         } catch (e: Exception) {
         }
 
-        if (!model.image_defult.isNullOrEmpty()) {
-//            item.image_stock.visibility = View.VISIBLE
+        if (!model.image_defult.isNullOrEmpty()){
             Glide.with(context).load(model.image_defult).into(item.image_stock)
-        } else item.image_stock.setBackgroundResource(R.color.back_hint)
+            item.image_stock.scaleType = ImageView.ScaleType.CENTER_CROP
+        }else{
+            Glide.with(context).load(context.getDrawable(R.drawable.pic_picture)).into(item.image_stock)
+            item.image_stock.setBackgroundResource(R.color.back_order)
+            item.image_stock.scaleType = ImageView.ScaleType.FIT_CENTER
+
+            val param =  item.image_stock.layoutParams as ViewGroup.MarginLayoutParams
+            param.setMargins(5,4,5,4)
+            item.image_stock.layoutParams = param
+        }
 
 
         item.setOnClickListener {
